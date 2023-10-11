363ec2043852342c282b4a34dd089f660af3378b
Hyang <thakd7092@naver.com>
2023-10-11 16:42:08 +0900

    Markdown 작업중

--- src/Page/BlogPage.jsx
+++ src/Page/BlogPage.jsx
@@ -2,12 +2,13 @@ import { useState } from "react";
 import BlogSkin1Side from "../Blog/BlogSkin1Side";
 import BlogSkin1Header from "../Blog/BlogSkin1Header";
 import BlogSkin2 from "../Blog/BlogSkin2";
+import OverView from "../Blog/OverView";
 import styled from "styled-components";
 
 
 function BlogPage(props){
     const [menuIndex, setMenuIndex] = useState(1)
-    const [skin, setSkin] = useState(1)
+    const [skin, setSkin] = useState(2)
 
     /** props로 넘어갈 state set 함수 */
     const ChangeMenuIndex = (value) => {
@@ -23,16 +24,18 @@ function BlogPage(props){
                 </BlogSection>
                 :
                 skin === 2 ?
-                    <BlogSkin2 menuIndex={menuIndex} changeMenuIndex={ChangeMenuIndex}>
-                    </BlogSkin2>
+                    <BlogSkin2 menuIndex={menuIndex} changeMenuIndex={ChangeMenuIndex} />
                     : null
             }
+            <OverView /> 
         </>
     )
 }
 const BlogSection = styled.div`
     display: flex;
     flex-direction: row;
-        `
+`
+
+
 
 export default BlogPage
