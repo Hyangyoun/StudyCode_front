cbf6c4eb9b9fe56a817c6762e292f3a9cdeec90d
usewooseok97 <kwoos3333@gmail.com>
2023-10-27 13:40:44 +0900

    스킨 뷰어 하트 완성 대댓글 하는중
    10/27

--- src/Blog/Review.jsx
+++ src/Blog/Review.jsx
@@ -1,41 +1,57 @@
-import { useCallback, useEffect, useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 import styled from "styled-components"
-import Viewer from "./Viewer"
 
 function Review(props) {
 
     const amount = useRef()
 
-    const ChangeTop = () => props.ChangeTop(amount)
+    /** 리뷰의 맨위위치를 알려주는 함수 */
+    function ReviewTop(){
+        const ReviewHead = amount.current.offsetTop
+        return( props.ReviewTop(ReviewHead))
+    } 
 
     useEffect(
         () => {
-        setTimeout(ChangeTop,1000)
+        ReviewTop()
     })
+
+    /** 대댓글 생성 삭제 state */
+    const [reReview, setReReview] = useState()
+
+    const AddReReview = () => {
+        setReReview(
+            <div className="rereview">
+                <div className="reviewId"><img src="./image/icon/profile.png" alt="아이디프로필"/>댜대기1</div>
+                <textarea>안녕</textarea>
+                <div className="closeBtn">X</div>
+                <div>작성하기</div>
+            </div>
+        )
+    }
 
     return(
         <Reviews >
             <div ref={amount} className="reviewHead">댓글</div>
             <ul>
                 <li className="review">
-                    <div className="reviewid"><img src="./image/icon/profile.png" alt="아이디프로필"/>VKI9008</div>
+                    <div className="reviewId"><img src="./image/icon/profile.png" alt="아이디프로필"/>VKI9008</div>
                     <div className="content">
                         <img src="/image/icon/okTeakYeon.png" alt="내용이미지파일"/>
                         <div>그것도 너를향해</div>
                     </div>
                     <div className="reviewInfo">
                         <span className="reviewDate">2023.09.13</span>
-                        <div className="rereview">답글</div>
+                        <div className="rereviewBtn" onClick={AddReReview}>답글</div>
                         <div className="warning"><img src="/image/icon/warning.png" alt="신고버튼"/></div>
                     </div>
+                    <div>{reReview}</div>
                 </li>
             </ul>
-            <div>
-                <div>
-                    <div className="guest"><img src="./image/icon/profile.png" alt="게스트프로필"/>다대기1</div>
-                    <input type="text"/>
-                    <div>등록</div>
-                </div>
+            <div className="writeReview">
+                <div className="guest"><img src="./image/icon/profile.png" alt="게스트프로필"/>다대기1</div>
+                <textarea placeholder="댓글을 입력하세요"></textarea>
+                <div className="exportReview">등록</div>
             </div>
         </Reviews>
     )
@@ -43,8 +59,10 @@ function Review(props) {
 
 const Reviews = styled.div`
     background-color: var(--second2);
-    margin-top: 15px;
-    width: 100%; height: auto;
+    margin: 15px 0 30px 0;
+    width: 100%; 
+    height: auto;
+    border-radius: 5px;
 
     &>ul{
         width: 100%;
@@ -54,6 +72,7 @@ const Reviews = styled.div`
     }
 
     .reviewHead{
+        padding-left: 10px;
         font-size: 20px;
         font-weight: bold;
         border-bottom: 1px solid var(--second);
@@ -62,15 +81,15 @@ const Reviews = styled.div`
         padding-left: 110px;
         border-bottom: 1px solid var(--second);
     }
-    .reviewid{
-        width: 100%;
+    .reviewId{
+        width: fit-content;
         height: 20px;
         font-size: 15px;
         display: flex;
         align-items: center;
         margin: 10px 0;
+        cursor: pointer;
         &>img{
-            object-fit: fill;
             width: 20px;
             height: auto;
             margin-right: 20px;
@@ -86,11 +105,13 @@ const Reviews = styled.div`
         display: flex;
         flex-direction: row;
         align-items: center;
+        border-bottom: 1px solid var(--second);
+
     }
     .reviewDate{
         font-size: 9px;
     }
-    .rereview{
+    .rereviewBtn{
         width: 25px;
         height: 17px;
         font-size: 9px;
@@ -100,26 +121,132 @@ const Reviews = styled.div`
         border:1px solid var(--second);
         border-radius: 3px;
         margin: 0 20px 0 10px;
-        
+        cursor: pointer;
         &:hover{
             background-color: var(--primary);
             color: white;
         }
     }
-    .guest{
+    .warning{
+        cursor: pointer;
+    }
+
+    //대댓글 작성칸///////////////////////////////////////////////////
+
+    .rereview{
         width: 100%;
-        height: 20px;
+        height: auto;
+        min-height: 70px;
+        flex-direction: column;
+        display: flex;
+        margin:  10px 0;
+        position: relative;
+        overflow: hidden;
+        &>textarea{
+            margin-left: 40px;
+            padding: 35px 10px 0 10px;
+            font-size: 15px;
+            width: 90%;
+            min-height: 20px;
+            background-color: var(--background);
+            border: 1px solid var(--second);
+            outline: none;
+            resize: none;
+            overflow: hidden;
+        }
+    }
+    .guest{
+        width: 91% ;
+        height: 35px;
         font-size: 15px;
         display: flex;
         align-items: center;
-        margin: 10px 0;
+        position: absolute;
+        margin: 0;
+        top: 1px;
+        left: 50px;
+        background-color: var(--background);
+        z-index: 5;
         &>img{
             object-fit: fill;
             width: 20px;
             height: auto;
-            margin-right: 20px;
+            margin-right: 5px;
         }
-
+}
+    .exportReview{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 75px;
+        height: 20px;
+        background-color: var(--second);
+        color: white;
+        border-radius: 3px;
+        position: absolute;
+        top: 135px;
+        left: 89%;
+        cursor: pointer;
     }
+
+
+
+    //댓글 작성칸/////////////////////////////////////////////////////
+    .writeReview{
+        width: 100%;
+        height: auto;
+        min-height: 170px;
+        flex-direction: column;
+        display: flex;
+        margin:  35px 0;
+        position: relative;
+        overflow: hidden;
+        &>textarea{
+            margin-left: 40px;
+            padding: 35px 10px 0 10px;
+            font-size: 15px;
+            width: 90%;
+            min-height: 95px;
+            background-color: var(--background);
+            border: 1px solid var(--second);
+            outline: none;
+            resize: none;
+            overflow: hidden;
+        }
+    }
+    .guest{
+        width: 91% ;
+        height: 35px;
+        font-size: 15px;
+        display: flex;
+        align-items: center;
+        position: absolute;
+        margin: 0;
+        top: 1px;
+        left: 50px;
+        background-color: var(--background);
+        z-index: 5;
+        &>img{
+            object-fit: fill;
+            width: 20px;
+            height: auto;
+            margin-right: 5px;
+        }
+}
+    .exportReview{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 75px;
+        height: 20px;
+        background-color: var(--second);
+        color: white;
+        border-radius: 3px;
+        position: absolute;
+        top: 135px;
+        left: 89%;
+        cursor: pointer;
+    }
+
 `
 export default Review
